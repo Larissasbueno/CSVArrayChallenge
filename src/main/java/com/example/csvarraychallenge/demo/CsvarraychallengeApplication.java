bcce124831eec190825ed1beb6e43d76866c2d8e
Larissasbuenno <larissasbuenno@gmail.com>
2023-08-29 21:05:28 -0300

    Info: Project CSV Array Challenge

---
+++ src/main/java/com/example/csvarraychallenge/demo/CsvarraychallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.csvarraychallenge.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvarraychallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvarraychallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/csvarraychallenge/demo/CsvarraychallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csvarraychallenge.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvarraychallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
